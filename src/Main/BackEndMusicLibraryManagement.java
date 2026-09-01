a7336948f5ced61c7ed18c1b6c281a9c7c72e81d
Bình Đỗ <dothanhbinh102007@gmail.com>
2026-09-01 16:51:30 +0700

    [Clean Main + Song.txt]

--- src/Main/BackEndMusicLibraryManagement.java
+++ src/Main/BackEndMusicLibraryManagement.java
@@ -3,15 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Main;
-import controllers.SongControllers;
 import java.io.IOException;
 
 public class BackEndMusicLibraryManagement {
 
 
     public static void main(String[] args) throws IOException{
-        SongControllers songController = new SongControllers();
 
-        songController.songSearch();
     }
 }
